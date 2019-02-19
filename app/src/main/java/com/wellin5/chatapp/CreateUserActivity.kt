8eff0fd78942b39af907761b885a91339679ee8e
Anthony Ebin <anthonyebin@gmail.com>
2019-02-18 20:21:30 -0500

    create profile view

---
+++ app/src/main/java/com/wellin5/chatapp/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.wellin5.chatapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar (view: View) {
+
+    }
+
+    fun clickGenerateColor (view: View) {
+
+    }
+
+    fun clickCreateUser (view: View) {
+
+    }
+}
