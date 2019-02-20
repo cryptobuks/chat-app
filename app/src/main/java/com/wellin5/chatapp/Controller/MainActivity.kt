1503875e3cd1965dbbaa4db5a7a7e0d7732062a2
Anthony Ebin <anthonyebin@gmail.com>
2019-02-20 16:05:25 -0500

    channel creation dialog

--- app/src/main/java/com/wellin5/chatapp/Controller/MainActivity.kt
+++ app/src/main/java/com/wellin5/chatapp/Controller/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.wellin5.chatapp.Controller
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.wellin5.chatapp.R
 import com.wellin5.chatapp.Services.AuthService
 import com.wellin5.chatapp.Services.UserDataService
 import com.wellin5.chatapp.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_channel_dialog.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
@@ -36,6 +38,8 @@ class MainActivity : AppCompatActivity() {
 
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(
             BROADCAST_USER_DATA_CHANGE))
+
+        hideKeyboard()
     }
 
     private val userDataChangeReceiver = object : BroadcastReceiver() {
@@ -75,11 +79,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickAddChannel (view: View){
+        if(AuthService.isLoggedIn){
+            val builder = AlertDialog.Builder(this)
+            val dialogView = layoutInflater.inflate(R.layout.add_channel_dialog, null)
 
+            builder.setView(dialogView)
+                .setPositiveButton("Add", DialogInterface.OnClickListener {dialog, id ->
+                    val nameTextField = dialogView.findViewById<EditText>(R.id.txtAddChannelName)
+                    val descTextField = dialogView.findViewById<EditText>(R.id.txtAddChannelDescription)
+                    val channelName = nameTextField.text.toString()
+                    val description = descTextField.text.toString()
+
+                    hideKeyboard()
+                    // Create channel with the name and desc
+                })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
+
+                    hideKeyboard()
+                })
+                .show()
+        }
     }
 
     fun clickSendMessage (view: View) {
 
+    }
+
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 
 }
